1193ff3b409035c508cb81390e1e0637e984c64d
hideonbush3 <ppoii0961@gmail.com>
2023-05-18 16:56:43 +0900

    spring data jpa : zipcode -> repo -> test

---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/ZipcodeRepository.java
@@ -0,0 +1,20 @@
+package hideonbush3.springboot.semiprojectv7.repository;
+
+import hideonbush3.springboot.semiprojectv7.model.Zipcode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ZipcodeRepository
+        extends JpaRepository<Zipcode, Long> {
+
+    // 메서드 쿼리 : 메서드를 정해주면 나중에 자동으로 sql문으로 변환됨
+    // 주로 select 쿼리에 최적화돼있음
+    // 작성법 - find엔티티명by컬럼명
+    // select * from ZIPCODE - findZipcodebyzipcodeAll
+
+    @Query("from Zipcode where dong like %:dong%")
+    List<Zipcode> findZipcodeByDong(@Param("dong") String dong);
+}

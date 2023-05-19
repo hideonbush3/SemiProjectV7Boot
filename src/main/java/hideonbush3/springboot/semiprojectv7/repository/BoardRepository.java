b1909e3ec44272db6f6c7f4625015b875ff611e3
hideonbush3 <ppoii0961@naver.com>
2023-05-19 15:46:40 +0900

    게시판 본문 조회수 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/repository/BoardRepository.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/BoardRepository.java
@@ -2,9 +2,17 @@ package hideonbush3.springboot.semiprojectv7.repository;
 
 import hideonbush3.springboot.semiprojectv7.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardRepository
-        extends JpaRepository<Board, Long> { }
+        extends JpaRepository<Board, Long> {
+
+    // JpaRepository 에서는 DML 지원 안함
+    // 단, Modifying, Transactional등을 추가하면 사용가능!
+    @Modifying@Transactional
+    @Query("update Board set views = views + 1 where bno = :bno")
+    int countViewBoard(@Param("bno") long bno);
+}

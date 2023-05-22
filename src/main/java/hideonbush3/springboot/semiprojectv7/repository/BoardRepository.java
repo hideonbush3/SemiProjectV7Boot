686eaedca7e48f58cdde488cc63247a8a239e1c1
hideonbush3 <ppoii0961@gmail.com>
2023-05-22 10:57:01 +0900

    쿼리 메서드를 이용한 다중 검색기능 구현 1

--- src/main/java/hideonbush3/springboot/semiprojectv7/repository/BoardRepository.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/BoardRepository.java
@@ -1,11 +1,14 @@
 package hideonbush3.springboot.semiprojectv7.repository;
 
 import hideonbush3.springboot.semiprojectv7.model.Board;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 //public interface BoardRepository extends JpaRepository<Board, Long> {
 public interface BoardRepository extends PagingAndSortingRepository<Board, Long> {
@@ -19,4 +22,13 @@ public interface BoardRepository extends PagingAndSortingRepository<Board, Long>
     // 게시글 전체 수 조회
     //@Query("select ceil(count(bno)/25) from board")
     int countBoardBy();
+
+    // 게시글 검색
+    List<Board> findByTitle(Pageable paging, String fkey);
+
+    List<Board> findByTitleOrContent(Pageable paging, String fkey1, String fkey2);
+
+    List<Board> findByUserid(Pageable paging, String fkey);
+
+    List<Board> findByContent(Pageable paging, String fkey);
 }

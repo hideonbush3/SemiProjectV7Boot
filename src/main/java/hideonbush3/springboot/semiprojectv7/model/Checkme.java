b6e29aaade075b87630fcef59546e5f22fc45401
hideonbush3 <ppoii0961@naver.com>
2023-05-23 16:00:25 +0900

    회원가입시 실명확인을 위한 모델 클래스 작성 (유효성 검사 어노테이션 추가)

---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/model/Checkme.java
@@ -0,0 +1,20 @@
+package hideonbush3.springboot.semiprojectv7.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+public class Checkme {
+
+    @NotBlank(message = "필수항목")
+    private String name;
+
+    @NotBlank(message = "필수항목")
+    private String jumin1;
+
+    @NotBlank(message = "필수항목")
+    private String jumin2;
+}

b4092b026759a56d10095535fd3fb835c627ee87
hideonbush3 <ppoii0961@naver.com>
2023-05-24 18:17:12 +0900

    자료실 - Repository 구현

---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsRepository.java
@@ -0,0 +1,7 @@
+package hideonbush3.springboot.semiprojectv7.repository;
+
+import hideonbush3.springboot.semiprojectv7.model.Pds;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PdsRepository extends PagingAndSortingRepository<Pds, Long> {
+}

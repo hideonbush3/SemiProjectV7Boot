2e5b6aa2187260444e2b40b6d4310a20b2b4b351
hideonbush3 <ppoii0961@naver.com>
2023-05-24 18:16:05 +0900

    자료실 - DAO 구현

---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/dao/PdsDao.java
@@ -0,0 +1,8 @@
+package hideonbush3.springboot.semiprojectv7.dao;
+
+import hideonbush3.springboot.semiprojectv7.model.Pds;
+import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
+
+public interface PdsDao {
+    int insertPds(Pds pds);
+}
---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/dao/PdsDaoImpl.java
@@ -0,0 +1,20 @@
+package hideonbush3.springboot.semiprojectv7.dao;
+
+import hideonbush3.springboot.semiprojectv7.model.Pds;
+import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
+import hideonbush3.springboot.semiprojectv7.repository.PdsAttachRepository;
+import hideonbush3.springboot.semiprojectv7.repository.PdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("pdsdao")
+public class PdsDaoImpl implements PdsDao{
+    @Autowired
+    private PdsRepository pdsRepository;
+    @Override
+    public int insertPds(Pds pds) {
+        // 제목, 작성자, 본문을 pds 테이블에 저장한 뒤
+        // 저장시 생성된 pno 를 리턴함 - 그것을 PdsAttach 클래스의 pno값에 저장해서 쓰면 됨
+        return Math.toIntExact(pdsRepository.save(pds).getPno());
+    }
+}

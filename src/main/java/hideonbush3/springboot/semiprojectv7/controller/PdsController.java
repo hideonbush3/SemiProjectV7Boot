f3985c781b6ee233a6e46157b5e57da81eccefb7
hideonbush3 <ppoii0961@gmail.com>
2023-05-25 00:22:57 +0900

    사용하지 않는 라이브러리 import 삭제

--- src/main/java/hideonbush3/springboot/semiprojectv7/controller/PdsController.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/controller/PdsController.java
@@ -1,24 +1,16 @@
 package hideonbush3.springboot.semiprojectv7.controller;
 
-import hideonbush3.springboot.semiprojectv7.model.Board;
 import hideonbush3.springboot.semiprojectv7.model.Pds;
-import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
 import hideonbush3.springboot.semiprojectv7.service.PdsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/pds")

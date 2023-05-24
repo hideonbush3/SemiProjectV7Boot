f7bf639e23e1592b704a74080735107d34b14dae
hideonbush3 <ppoii0961@gmail.com>
2023-05-24 17:21:31 +0900

    자료실 - 글쓰기 폼/js 파일 작성

---
+++ src/main/resources/static/js/pds.js
@@ -0,0 +1,14 @@
+// -------------------------------------- 새글쓰기
+const writebtn = document.querySelector('#writebtn');
+writebtn?.addEventListener('click', () => {
+    const pdsfrm = document.forms.pdsfrm;
+    if(pdsfrm.title.value === '')    alert("제목을 입력하세요");
+    else if(pdsfrm.content.value === '') alert("본문을 입력하세요");
+    else if(grecaptcha.getResponse() === '') alert("자갑방 필수!");
+    else{
+        pdsfrm.method='post';
+        pdsfrm.enctype ='multipart/form-data';
+        pdsfrm.submit();
+    }
+})
+

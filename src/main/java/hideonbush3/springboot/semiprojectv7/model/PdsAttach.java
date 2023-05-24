d64eccb9abe97f26fef4b014866d7d43023a9fec
hideonbush3 <ppoii0961@gmail.com>
2023-05-24 17:11:07 +0900

    자료실 - PdsAttach Entity

---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/model/PdsAttach.java
@@ -0,0 +1,26 @@
+package hideonbush3.springboot.semiprojectv7.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "pdsattach")
+@Entity
+@Data
+public class PdsAttach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pano;
+
+    private String fname;
+    private String ftype;
+    private String fsize;
+    @Column(insertable = false, updatable = false)
+    private Integer fdowns;
+    private Integer pno;
+}

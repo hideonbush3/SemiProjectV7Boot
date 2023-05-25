e8f5fff67085fcd0785fe33579f3da0a048ea8cd
hideonbush3 <ppoii0961@gmail.com>
2023-05-25 10:58:05 +0900

    자료실 - PdsDao 수정 PdsAttach 저장 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsRepository.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsRepository.java
@@ -3,5 +3,5 @@ package hideonbush3.springboot.semiprojectv7.repository;
 import hideonbush3.springboot.semiprojectv7.model.Pds;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PdsRepository extends PagingAndSortingRepository<Pds, Long> {
+public interface PdsRepository extends PagingAndSortingRepository<Pds, Long>{
 }
---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsaRepository.java
@@ -0,0 +1,7 @@
+package hideonbush3.springboot.semiprojectv7.repository;
+
+import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsaRepository extends JpaRepository<PdsAttach, Long> {
+}

9633487f626f19bff9a213d7e0c795cff755bff6
hideonbush3 <ppoii0961@gmail.com>
2023-05-25 10:55:17 +0900

    자료실 - PdsService 수정 PDS 저장후 리턴타입 변경 PdsAttach 첨부파일 업로드 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/service/PdsServiceImpl.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/service/PdsServiceImpl.java
@@ -2,9 +2,13 @@ package hideonbush3.springboot.semiprojectv7.service;
 
 import hideonbush3.springboot.semiprojectv7.dao.PdsDao;
 import hideonbush3.springboot.semiprojectv7.model.Pds;
+import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service("pdssrv")
 public class PdsServiceImpl implements PdsService{
@@ -15,13 +19,26 @@ public class PdsServiceImpl implements PdsService{
     private PdsUtils pdsUtils;
 
     @Override
-    public int newPds(Pds pds) {
-        pds.setUuid(pdsUtils.makeUUID());
-        return pdsdao.insertPds(pds);
+    public Map<String, Object> newPds(Pds pds) {
+        pds.setUuid(pdsUtils.makeUUID());   // 유니크한 문자열 생성
+        int pno = pdsdao.insertPds(pds);    // 일반폼 데이터 디비에 저장
+
+        // 첨부파일을 시스템에 저장할때 사용할 정보를 Map에 저장
+        Map<String, Object> pinfo = new HashMap<>();
+        pinfo.put("pno", pno);
+        pinfo.put("uuid", pds.getUuid());
+        return pinfo;
     }
 
     @Override
-    public boolean newPdsAttach(MultipartFile attach, int pno) {
-        return true;
+    public boolean newPdsAttach(
+            MultipartFile attach, Map<String, Object> pinfo) {
+
+        // 첨부파일 업로드 처리
+        PdsAttach pa = pdsUtils.processUpload(attach, pinfo);
+        // 첨부파일 정보 디비에 저장
+        int pano = pdsdao.insertPdsAttach(pa);
+
+        return (pano > 0) ? true : false;
     }
 }

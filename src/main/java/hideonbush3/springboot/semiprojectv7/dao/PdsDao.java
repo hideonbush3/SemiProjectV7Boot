6bc0490a0c001b6634c7319a68ee6d47b6e36b59
hideonbush3 <ppoii0961@gmail.com>
2023-05-25 11:55:38 +0900

    자료실 - 목록보기 DAO 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/dao/PdsDao.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/dao/PdsDao.java
@@ -3,8 +3,12 @@ package hideonbush3.springboot.semiprojectv7.dao;
 import hideonbush3.springboot.semiprojectv7.model.Pds;
 import hideonbush3.springboot.semiprojectv7.model.PdsAttach;
 
+import java.util.Map;
+
 public interface PdsDao {
     int insertPds(Pds pds);
 
     int insertPdsAttach(PdsAttach pa);
+
+    Map<String, Object> selectPds(int i);
 }

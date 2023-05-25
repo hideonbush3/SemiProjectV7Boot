7639d986657c627c7de5e5b4bb663518c27bf222
hideonbush3 <ppoii0961@gmail.com>
2023-05-25 15:09:02 +0900

    자료실 - 본문보기 조회수 증가 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsRepository.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsRepository.java
@@ -1,7 +1,15 @@
 package hideonbush3.springboot.semiprojectv7.repository;
 
 import hideonbush3.springboot.semiprojectv7.model.Pds;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PdsRepository extends PagingAndSortingRepository<Pds, Long>{
+
+    @Modifying@Transactional
+    @Query("update Pds set views = views + 1 where pno = :pno")
+    void countViewById(@Param("pno") long pno);
 }

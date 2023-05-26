1e636e9f2e58540608e070c0cd98a318739bb755
hideonbush3 <ppoii0961@gmail.com>
2023-05-26 16:09:10 +0900

    자료실 댓글 - 댓글쓰기 repository 구현

--- src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -2,10 +2,18 @@ package hideonbush3.springboot.semiprojectv7.repository;
 
 import hideonbush3.springboot.semiprojectv7.model.PdsReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
 
 public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(Long pno);
+
+    @Modifying@Transactional
+    @Query(value = "update pdsreply set refno = :rpno where rpno = :rpno", nativeQuery = true)
+    void updateRefno(@Param("rpno") Long rpno);
 }

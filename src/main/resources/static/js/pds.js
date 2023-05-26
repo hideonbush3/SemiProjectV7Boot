3caf5baab50f58dc614bc160c4dae0c6a1327398
hideonbush3 <ppoii0961@naver.com>
2023-05-26 17:49:06 +0900

    자료실 댓글 - 대댓글쓰기 폼 구현

--- src/main/resources/static/js/pds.js
+++ src/main/resources/static/js/pds.js
@@ -36,3 +36,34 @@ rpnewbtn?.addEventListener('click', () => {
         rpfrm.submit();
     }
 })
+
+let modal = null;
+// [추가] 링크 클릭시 대댓글 작성 모달 띄우기
+const showComment = (refno) => {
+    const frefno = document.querySelector('#refno');
+    const cmtModal = document.querySelector("#cmtModal");
+
+    frefno.value = refno;
+
+    let mymodal = null;
+    try{
+        mymodal = new bootstrap.Modal(cmtModal, {});
+        modal = mymodal;
+    }catch (e){}
+
+    mymodal.show();
+}
+
+const cmtbtn = document.querySelector("#cmtbtn");
+cmtbtn?.addEventListener('click', () => {
+    const cmtfrm = document.forms.cmtfrm;
+    if(cmtfrm.reply.value === '') alert("댓글을 작성하세요");
+    else if(cmtfrm.userid.value === '') alert("작성자가 없어요");
+    else if(cmtfrm.pno.value === '') alert("본문글 번호가 없어요");
+    else if(cmtfrm.refno.value === '') alert("댓글번호가 없어요");
+    else{
+        cmtfrm.method = 'post';
+        cmtfrm.action = '/pds/rreplyok';
+        cmtfrm.submit();
+    }
+})

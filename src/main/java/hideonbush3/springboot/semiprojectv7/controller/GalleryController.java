eb2521e15276e7f19d3213d13227a430de7989e0
hideonbush3 <ppoii0961@naver.com>
2023-06-07 11:18:15 +0900

    갤러리 - 이미지 갤러리 자바스크립트

--- src/main/java/hideonbush3/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/controller/GalleryController.java
@@ -1,8 +1,15 @@
 package hideonbush3.springboot.semiprojectv7.controller;
 
+import hideonbush3.springboot.semiprojectv7.model.Pds;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/gallery")
@@ -11,6 +18,21 @@ public class GalleryController {
     @GetMapping("/list")
     public String list() {
         return "gallery/list";
+    }
+
+    @GetMapping("/write")   // 입력폼
+    public String write(Model m){
+        m.addAttribute("gallery", new Gallery());
+        return "gallery/write";
+    }
+
+    @GetMapping("/view")
+    public String view(int pno, Model m){
+//        m.addAttribute("gallery", pdssrv.readOnePds(pno));
+//        m.addAttribute("attach", pdssrv.readOnePdsAttach(pno));
+//        m.addAttribute("rplist", pdssrv.readPdsReply(pno));
+
+        return "gallery/view";
     }
 
 }

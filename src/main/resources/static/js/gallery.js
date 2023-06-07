7efa4fc6dc37e33045ab3d38ba3c4ce2ccf890ce
hideonbush3 <ppoii0961@naver.com>
2023-06-07 12:13:21 +0900

    갤러리 - 첨부파일이 이미지 파일인지 유효성 검증

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -3,8 +3,25 @@ const writebtn = document.querySelector('#writebtn');
 
 // 이미지 첨부 조건 검사
 const checkAttachs = () => {
+    const attachs = document.querySelector('#attachs')
+    let result = false;
+    // 이미지 첨부파일이 하나 이상일때
+    if('files' in attachs && attachs.files.length > 0){
+        // 첨부파일들이 이미지인지 검사
+        for(attach of attachs.files){
+            // console.log(attach.name + ',' + attach.type
+            //     + ',' + attach.size)
+            // 이미지 파일의 MIME 형식 : image/jpg, image/jpeg, image/png, image/gif
+            if(!attach.type.startsWith("image")){
+                alert("첨부파일중 이미지 파일이 아닌 파일이 존재합니다");
+                return false;
+            }
+        }
+    }else{
+        alert("하나 이상의 이미지를 선택하세요")
+    }
 
-    return false;
+    return result;
 };
 
 writebtn?.addEventListener('click', () => {

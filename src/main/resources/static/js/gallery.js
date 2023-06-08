81bff1e584e87776a82e36cfefb0266485809671
hideonbush3 <ppoii0961@naver.com>
2023-06-08 09:49:27 +0900

    갤러리 - 프론트단 수정

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -4,7 +4,7 @@ const writebtn = document.querySelector('#writebtn');
 // 이미지 첨부 조건 검사
 const checkAttachs = () => {
     const attachs = document.querySelector('#attachs')
-    let result = false;
+    let checkOk = true;
     // 이미지 첨부파일이 하나 이상일때
     if('files' in attachs && attachs.files.length > 0){
         // 첨부파일들이 이미지인지 검사
@@ -14,14 +14,15 @@ const checkAttachs = () => {
             // 이미지 파일의 MIME 형식 : image/jpg, image/jpeg, image/png, image/gif
             if(!attach.type.startsWith("image")){
                 alert("첨부파일중 이미지 파일이 아닌 파일이 존재합니다");
-                return false;
+                checkOk =  false;
             }
         }
     }else{
-        alert("하나 이상의 이미지를 선택하세요")
+        alert("하나 이상의 이미지를 선택하세요");
+        checkOk = false;
     }
 
-    return result;
+    return checkOk;
 };
 
 writebtn?.addEventListener('click', () => {

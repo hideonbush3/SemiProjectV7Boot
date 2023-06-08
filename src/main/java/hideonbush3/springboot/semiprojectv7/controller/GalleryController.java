d9368ab8aa17505e4345b3aefccd6507a905d1a4
hideonbush3 <ppoii0961@naver.com>
2023-06-08 09:48:05 +0900

    갤러리 - 백단 로직 추가

--- src/main/java/hideonbush3/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/controller/GalleryController.java
@@ -1,7 +1,8 @@
 package hideonbush3.springboot.semiprojectv7.controller;
 
 import hideonbush3.springboot.semiprojectv7.model.Gallery;
-import hideonbush3.springboot.semiprojectv7.model.Pds;
+import hideonbush3.springboot.semiprojectv7.service.GalleryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,31 +10,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
+
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
+
+    @Autowired
+    GalleryService galsrv;
 
     @GetMapping("/list")
     public String list() {
         return "gallery/list";
     }
 
-    @GetMapping("/write")   // 입력폼
-    public String write(Gallery gallery, List<MultipartFile> attachs){
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("gallery", new Gallery());
+
         return "gallery/write";
     }
 
-    @GetMapping("/view")
-    public String view(int pno, Model m){
-//        m.addAttribute("gallery", pdssrv.readOnePds(pno));
-//        m.addAttribute("attach", pdssrv.readOnePdsAttach(pno));
-//        m.addAttribute("rplist", pdssrv.readPdsReply(pno));
+    @PostMapping("/write")
+    public String writeok(Gallery gallery, List<MultipartFile> attachs) {
+        String viewPage = "error";
 
-        return "gallery/view";
+        if (!attachs.isEmpty()) {// 첨부파일이 존재한다면
+            Map<String, Object> ginfo = galsrv.newGallery(gallery);
+            galsrv.newGalAttach(attachs, ginfo);
+            viewPage = "redirect:/gallery/list";
+        }
+
+        return viewPage;
     }
+
 
 }
---
+++ src/main/java/hideonbush3/springboot/semiprojectv7/service/GalleryService.java
@@ -0,0 +1,12 @@
+package hideonbush3.springboot.semiprojectv7.service;
+
+import hideonbush3.springboot.semiprojectv7.model.Gallery;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+public interface GalleryService {
+    Map<String, Object> newGallery(Gallery gallery);
+    boolean newGalAttach(List<MultipartFile> attachs, Map<String, Object> ginfo);
+}

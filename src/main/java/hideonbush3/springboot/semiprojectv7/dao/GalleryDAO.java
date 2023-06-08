eed56fd9f55cb78fbde0abc0ab9bd40d52242d13
hideonbush3 <ppoii0961@naver.com>
2023-06-08 11:46:36 +0900

    갤러리 - dao 수정

--- src/main/java/hideonbush3/springboot/semiprojectv7/dao/GalleryDAO.java
+++ src/main/java/hideonbush3/springboot/semiprojectv7/dao/GalleryDAO.java
@@ -3,8 +3,12 @@ package hideonbush3.springboot.semiprojectv7.dao;
 import hideonbush3.springboot.semiprojectv7.model.GalAttach;
 import hideonbush3.springboot.semiprojectv7.model.Gallery;
 
+import java.util.Map;
+
 public interface GalleryDAO {
     int insertGal(Gallery gallery);
 
     int insertGalAttach(GalAttach ga);
+
+    Map<String, Object> selectGallery(Integer cpg);
 }
